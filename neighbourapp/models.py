4083e62960123f04a27a303d9a19b36f370e6c01
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-24 18:34:28 +0300

    adding an admin user admin foreign keys

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -17,9 +17,22 @@ class NeighbourHood(models.Model):
     login = models.CharField(max_length=200)
     occupation_count = models.IntegerField(default=0)
     # admin = models.ForeignKey
+    def __str__(self):
+        return self.name
 
 class User(models.Model):
     name = models.CharField(max_length=255)
     id = models.IntegerField()
     neighborhood_id = models.ForeignKey(NeighbourHood,on_delete= models.CASCADE)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Business(models.Model):
+    name = models.CharField(max_length=25)
+    user= models.ForeignKey(User, on_delete= models.CASCADE)
+    neighborhood_id = models.ForeignKey(NeighbourHood,on_delete= models.CASCADE)
+    bussiness_email= models.EmailField()    
+    def __str__(self):
+        return self.name
