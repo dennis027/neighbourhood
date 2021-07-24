59c1630894323e2c7b9bb0e7a195ccbe216fe2c6
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-24 18:23:07 +0300

    adding a neighbourhood class

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -12,8 +12,14 @@ class Post(models.Model):
     def publication_date(self):
         return self.pub_date.strftime('%b %e, %Y')
 
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=255)
+    login = models.CharField(max_length=200)
+    occupation_count = models.IntegerField(default=0)
+    # admin = models.ForeignKey
+
 class User(models.Model):
     name = models.CharField(max_length=255)
     id = models.IntegerField()
-    neighborhood_id = models.ForeignKey()
+    neighborhood_id = models.ForeignKey(NeighbourHood,on_delete= models.CASCADE)
     email = models.EmailField()
