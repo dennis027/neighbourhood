e0d70b79830bc6ab60d9c74638796eea160dfb24
Dennis <dennis.kimani@student.moringaschool.com>
2021-07-24 17:48:56 +0300

    updating my app

--- neighbourapp/urls.py
+++ neighbourapp/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf import settings
 
-urlpatterns = [
-
-path(r'',views.index,name='index')
+urlpatterns=[
+    url(r'^$',views.index, name='index'),
+    url('register/',views.register, name='registration'),
+    url('login/', auth_views.LoginView.as_view(), name='login'),
+    url(r'profile/', views.profile, name='profile'),
+    url(r'^create/', views.create, name="create"),
+    
 ]
